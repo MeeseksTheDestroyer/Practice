cd5b155b1c05a710ebc4ca8374dd93cd7df7917f
unknown <fifakawior@gmail.com>
2023-05-10 14:29:26 +0200

    Second

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,3 +5,21 @@ MenuIcon.addEventListener("click", function(){
     document.querySelector("header").classList.toggle("header-show");
     
 })
+
+let quest = document.querySelectorAll(".question");
+for (let i=0; i < quest.length; i++) {
+    quest[i].addEventListener("click", function(e) {
+        if(this.children[0].children[1].children[0].style.transform == "rotate(-45deg)") 
+        {
+            this.children[0].children[1].children[0].style.transform = "";
+            this.children[0].children[1].children[0].classList.remove("rotation");
+            this.children[1].style.display = "none";
+        }
+        else {
+            this.children[0].children[1].children[0].classList.add("rotation");
+            this.children[0].children[1].children[0].style.transform = "rotate(-45deg)";
+            this.children[1].style.display = "block";
+        }
+        
+    })
+}
