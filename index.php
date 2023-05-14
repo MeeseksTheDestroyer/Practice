d33d9b7c3856ace8bed440b9cb78ed10822c14ef
MeeseksTheDestroyer <fifakawior@gmail.com>
2023-05-14 23:02:25 +0200

    2. layout

--- index.php
+++ index.php
@@ -5,18 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dlaosadzonych</title>
-    <link rel="stylesheet" href="assets/styles/font/style.css">
+    
     <link rel="stylesheet" href="assets/styles/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
 
 <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital@1&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="menu">
+<div class="menu">
         <div class="list">
         <a  href="http://localhost/Practice#o-nas">O nas</a>
             <a   href="http://localhost/Practice/wypiska">Wpłać wypiskę</a>
@@ -24,58 +24,114 @@
             <a   href="http://localhost/Practice#o-nas">Strona główna</a>
         </div>
     </div>
-    <section class="top">
-        <div>Serwis dla rodzin osób osadzonych</div>
-        <div><a href="mailto:kontakt@gmail.com">kontakt@gmail.com</a></div>
-    </section>
-    <header>
-        <div>
-            <a href="http://localhost/Practice/">
-            <img height="60px" src="https://www.zpozdrowieniem.pl/wp-content/themes/zpozdrowieniem.dev/images/logo.png">
+    <div class="container-absolute">
+
+        <section class="top">
+            <div>Serwis dla rodzin osób osadzonych</div>
+            <div><a href="mailto:kontakt@gmail.com">kontakt@gmail.com</a></div>
+        </section>
+        <header>
+            <div>
+                <a href="http://localhost/Practice/">
+                <img id="logo" class="logo-white" height="60px" src="https://www.zpozdrowieniem.pl/wp-content/themes/zpozdrowieniem.dev/images/logo.png">
+                </a>
+            </div>
+            <nav>
+            <a class="hover-underline-animation" href="http://localhost/Practice#o-nas">O nas</a>
+                <a class="hover-underline-animation"  href="http://localhost/Practice/wypiska">Wpłać wypiskę</a>
+                <a class="hover-underline-animation"  href="http://localhost/Practice#top">Kontakt</a>
+                <a class="hover-underline-animation"  href="http://localhost/Practice#o-nas">Strona główna</a>
+                <svg class="icon-menu logo-white" height="30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+            </nav>
+        </header>
+    </div>
+    <div class="slider-img">
+        <div class="title">
+            <div>
+                <h2>
+                    Kara pozbawienia wolności.
+
+                </h2><br>
+                <div>
+                    Zakład Karny – poznaj prawa i obowiązki więźniów!
+                </div>
+            </div>
+            
+        </div>  
+        <div class="buttons">
+            <a href="">Dowiedz się więcej</a>
+            <a href="">Wyślij wypiskę</a>
+        </div>
+    </div>
+    <section class="about-us">
+        <h1>
+            <a class="blue-mark">Kim</a> jesteśmy?
+
+        </h1>
+        <div class="center">
+            <a class="red-mark"></a>
+        </div>
+        <div class="center">
+            <a class="center-a" style="text-align:center; padding-top:20px">
+                Ekipa serwisu zpozdrowieniem.pl pomoże Państwu w przesłaniu wypiski (środków pieniężnych do dyspozycji osoby osadzonej) oraz złożeniu zamówień na paczkę żywnościową i paczkę higieniczną do zakładu karnego lub aresztu śledczego.
+                Oferujemy również możliwość skorzystania z profesjonalnej porady prawnej. 
             </a>
         </div>
-        <nav>
-        <a class="hover-underline-animation" href="http://localhost/Practice#o-nas">O nas</a>
-            <a class="hover-underline-animation"  href="http://localhost/Practice/wypiska">Wpłać wypiskę</a>
-            <a class="hover-underline-animation"  href="http://localhost/Practice#top">Kontakt</a>
-            <a class="hover-underline-animation"  href="http://localhost/Practice#o-nas">Strona główna</a>
-            <svg class="icon-menu" height="30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
-        </nav>
-    </header>
-    <main>
-        <section class="main-container">
-            <section>
-                <div class="main-about-us" id="o-nas">
-
-
-                    <a class="quest">Kim jestesmy?</a>
-                    <br><br>
-                    <a class="transparency">
-                        PACZKA I WYPISKA DLA OSADZONEGO - ZPOZDROWIENIEM.PL
-                    </a>
-                    <br>
-                    <a class="about-us-content">
-                        
-                        Ekipa serwisu zpozdrowieniem.pl pomoże Państwu w przesłaniu wypiski (środków pieniężnych do dyspozycji osoby osadzonej) oraz złożeniu zamówień na paczkę żywnościową i paczkę higieniczną do zakładu karnego lub aresztu śledczego.
-                        Oferujemy również możliwość skorzystania z profesjonalnej porady prawnej.
-                    </a>
-                    <br><br><br><br>
-                    <a href="http://localhost/Practice/wypiska" class="btn">
-                        Wyślij wypiskę
-                    </a>
+    </section>
+    <div class="quest-mark">
+        <h1>
+            Popularne <a class="white-mark">pytania</a>
     
+        </h1>
+        <div class="center">
+            <a class="red-mark"></a>
+        </div>
+    </div>
+
+    <section class="info-container" id="quest-link">
+        <div class="questions">
+            <div class="question" style="border-top: .5px solid white">
+                <div class="question-title">
+                    <div class="quest1">Jak szybko wysyłane są środki pieniężne dla osoby osadzonej?</div> 
+                    <div class="quest2"><span  class="fa-solid fa-angle-down" style="color:white; font-size: larger; font-weight: bold">&#43;</span></div>
                 </div>
-            </section>
-
-                        <div class="main-image">
-                            <div class="image">
                 
-                            </div>
-                        </div>
-        </section>
+                <div class="question-description">
+                    Pieniądze wysyłamy tego samego dnia – jeżeli zamówienie zostało złożone do godziny 12:00 i poprawnie opłacone przez Przelewy24. Jeżeli zamówienie złożycie Państwo później to najczęściej też wyślemy środki tego samego dnia – jednak nie możemy tego zagwarantować np. w przypadku dużej ilości zamówień.
+                </div>
+            </div>
+            <div class="question">
+                <div class="question-title">
+                    <div class="quest1">Kto może wysłać pieniądze dla osoby osadzonej?</div> 
+                    <div class="quest2"><span  class="fa-solid fa-angle-down" style="color:white; font-size: larger; font-weight: bold">&#43;</span></div>
+                </div>
+                
+                <div class="question-description">
+                    Środki pieniężne dla osoby osadzonej może wysłać każdy.
+                </div>
+            </div>
+            <div class="question">
+                <div class="question-title">
+                    <div class="quest1">Co ze zwrotami pieniędzy w przypadku transportu osoby osadzonej lub podanych złych danych?</div> 
+                    <div class="quest2"><span  class="fa-solid fa-angle-down" style="color:white; font-size: larger; font-weight: bold">&#43;</span></div>
+                </div>
+                
+                <div class="question-description">
+                    Oczywiście mają Państwo gwarancję zwrotu środków za wypiskę / paczkę żywnościową (wartości zamówienia). Po zaksięgowaniu zwrotu z jednostki penitencjarnej i/lub kantyny kontaktujemy się z jednostką w celu ustalenia przyczyny zwrotu i niezwłocznie po otrzymaniu informacji otrzymują Państwo maila z uzasadnieniem. Do przykładowych powodów możemy zaliczyć np. transport osoby osadzonej, kara na otrzymanie paczki żywnościowej, brak uprawnień do sporządzenia zamówienia na paczkę żywnościową, brak osadzonego/osadzonej w jednostce itp.
+                </div>
+            </div>
 
-    </main>
+        </div>
+    </section>
+    <div class="quest-mark white">
+        <h1>
+            Dlaczego <a class="blue-mark">my?</a>
     
+        </h1>
+        <div class="center">
+            <a class="red-mark"></a>
+        </div>
+    </div>
     <section class="advantages">
         <section class="advantages-container">
             <div class="advantage">
@@ -123,49 +179,40 @@
             
         </section>
     </section>
-
-    <section class="title">
-        Popularne pytania
-    </section>
-    <section class="info-container" id="quest-link">
-        <div class="questions">
-            <div class="question" style="border-top: .5px solid gray">
-                <div class="question-title">
-                    <div class="quest1">Jak szybko wysyłane są środki pieniężne dla osoby osadzonej?</div> 
-                    <div class="quest2"><span  class="fa-solid fa-angle-down" style="color:black; font-size: larger; font-weight: bold">&#43;</span></div>
-                </div>
-                
-                <div class="question-description">
-                    Pieniądze wysyłamy tego samego dnia – jeżeli zamówienie zostało złożone do godziny 12:00 i poprawnie opłacone przez Przelewy24. Jeżeli zamówienie złożycie Państwo później to najczęściej też wyślemy środki tego samego dnia – jednak nie możemy tego zagwarantować np. w przypadku dużej ilości zamówień.
-                </div>
-            </div>
-            <div class="question">
-                <div class="question-title">
-                    <div class="quest1">Kto może wysłać pieniądze dla osoby osadzonej?</div> 
-                    <div class="quest2"><span  class="fa-solid fa-angle-down" style="color:black; font-size: larger; font-weight: bold">&#43;</span></div>
-                </div>
-                
-                <div class="question-description">
-                    Środki pieniężne dla osoby osadzonej może wysłać każdy.
-                </div>
-            </div>
-            <div class="question">
-                <div class="question-title">
-                    <div class="quest1">Co ze zwrotami pieniędzy w przypadku transportu osoby osadzonej lub podanych złych danych?</div> 
-                    <div class="quest2"><span  class="fa-solid fa-angle-down" style="color:black; font-size: larger; font-weight: bold">&#43;</span></div>
-                </div>
-                
-                <div class="question-description">
-                    Oczywiście mają Państwo gwarancję zwrotu środków za wypiskę / paczkę żywnościową (wartości zamówienia). Po zaksięgowaniu zwrotu z jednostki penitencjarnej i/lub kantyny kontaktujemy się z jednostką w celu ustalenia przyczyny zwrotu i niezwłocznie po otrzymaniu informacji otrzymują Państwo maila z uzasadnieniem. Do przykładowych powodów możemy zaliczyć np. transport osoby osadzonej, kara na otrzymanie paczki żywnościowej, brak uprawnień do sporządzenia zamówienia na paczkę żywnościową, brak osadzonego/osadzonej w jednostce itp.
-                </div>
-            </div>
-
-        </div>
-    </section>
     <section class="przelewy24">
         <img src="assets/images/przelewy24.png">
     </section>
     <footer><a>ZPOZDROWIENIEM.PL© | 2023</a><a href="#"> Polityka prywatności</a><a href="#">Regulamin</a></footer>
+    <script>
+        var MenuIcon = document.querySelector(".icon-menu");
+
+        MenuIcon.addEventListener("click", function(){
+            document.querySelector(".menu").classList.toggle("show");
+            document.querySelector("header").classList.toggle("header-show");
+            document.querySelector("#logo").classList.toggle("logo-white");
+            document.querySelector(".icon-menu").classList.toggle("logo-white");
+        })
+
+        let quest = document.querySelectorAll(".question");
+        for (let i=0; i < quest.length; i++) {
+            quest[i].addEventListener("click", function(e) {
+                if(this.children[0].children[1].children[0].style.transform == "rotate(-45deg)") 
+                {
+                    this.children[0].children[1].children[0].style.transform = "";
+                    this.children[0].children[1].children[0].classList.remove("rotation");
+                    this.children[1].style.display = "none";
+                }
+                else {
+                    this.children[0].children[1].children[0].classList.add("rotation");
+                    this.children[0].children[1].children[0].style.transform = "rotate(-45deg)";
+                    this.children[1].style.display = "block";
+                }
+                
+            })
+        }
+
+       
+    </script>
     <script src="assets/scripts/script.js"></script>
     <script>
         alert("Nie jest to finalna wersja strony, rzeczy do zrobienia w pliku Do_zrobienia.txt");
